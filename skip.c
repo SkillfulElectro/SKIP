3ec6431968d5372add24dc0b7e63170e3cc0e5f9
ElectroMutex <97535877+SkillfulElectro@users.noreply.github.com>
2025-11-27 16:19:21 +0330

    Import header body buffer error added

--- skip.c
+++ skip.c
@@ -334,6 +334,11 @@ int skip_import_header_body(void* cfg, const char* buffer, uint64_t buffer_size)
 
     const char* current_pos = buffer;
     const char* end_pos = buffer + buffer_size;
+
+    if (buffer_size % (sizeof(int32_t) + sizeof(uint64_t)) {
+        return SKIP_ERROR_INVALID_ARGUMENT;
+    }
+    
     int system_endian = skip_get_system_endian();
     int config_endian = config->endian;
 
